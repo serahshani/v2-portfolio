79a3859773f0f9ac236adac37be3ecb26e096406
serah shani <serahshani035@gmail.com>
2024-12-13 11:47:11 +0300

    updated some details

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+
+const logoimage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconsdb.com%2Fwhite-icons%2Fcopyright-icon.html&psig=AOvVaw3JKAXfdg_PYFtLxb5POgVh&ust=1726132790314000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDgg5HIuogDFQAAAAAdAAAAABAE"
+const meta = {
+    title: "Serah Shani",
+    description: "I’m Serah Shani_ Full stack devloper,currently working in Kenya",
+};
+
+const introdata = {
+    title: "I’m Serah Shani",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am Serah Shani, a full stack developer who is passionate about technology.",
+    your_img_url: "https://images.unsplash.com/photo-1623520333087-62b8793e3d23?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I specialize in crafting elegant and efficient code to solve complex problems. My expertise spans multiple programming languages and frameworks, including [list a few relevant technologies, e.g., JavaScript, Python, React, Node.js]. I thrive on transforming ideas into high-quality applications and am committed to continuous learning and adapting to new technologies. With a keen eye for detail and a collaborative mindset, I excel at working in agile environments to deliver innovative solutions that drive business success.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Front-end Developer",
+        where: "Git Software Solutions",
+        date: "2023-Present",
+    },
+    {
+        jobtitle: "Front-end Developer",
+        where: "Swahili pot hub",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 60,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Custom Software Development",
+        description: "Overview: Crafting bespoke software solutions tailored to your business needs and objectives.Key Offerings:Web applications Mobile applications (iOS & Android) Desktop applications API development and integration",
+    },
+    {
+        title: "Front-End Development",
+        description: "Overview: Creating engaging and responsive user interfaces with a focus on usability and performance. Key Offerings: HTML, CSS, and JavaScript development React, Vue.js, or Angular applications UI/UX design implementation Cross-browser compatibility and responsive design",
+    },
+    {
+        title: "Back-End Development",
+        description: "Overview: Building robust and scalable server-side solutions to support your application’s functionality. Key Offerings: Server-side programming (Node.js, Python, Ruby, etc.) Database design and management (SQL, NoSQL) API and microservices development Security and performance optimization",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "serahshani035@gmail.com",
+    YOUR_FONE: "+254758056305",
+    description: "For more inquiries kindly reach out via the contacts above ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/serahshani",
+    facebook: "https://www.facebook.com/profile.php?id=100086690103896",
+    linkedin: "https://www.linkedin.com/in/serah-shani-947131222/",
+    twitter: "https://x.com/shedev22",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logoimage,
+};
